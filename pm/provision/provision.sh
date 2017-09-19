e82d6ee6fa700191225b89296ddf9b40fd48e900
hcrisbuc <cb@bucerzan.com>
2017-09-19 22:08:41 +0100

    adding vagrant puppet master

---
+++ pm/provision/provision.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# vi /etc/sysconfig/network-scripts/ifcfg-eth0
+
+sudo su
+echo '''DEVICE="eth1"
+        ONBOOT="yes"
+	        TYPE="Ethernet"
+		        BOOTPROTO="static"
+			        IPADDR="192.168.56.215"
+				        NETMASK="255.255.255.0" ''' > /etc/sysconfig/network-scripts/ifcfg-eth1
+
+					        /etc/init.d/network restart
+
+						    echo '192.168.56.215 CentInf01' >> /etc/hosts
