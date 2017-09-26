cf724ac45ac7e843d54d5a38ac96caff9d05e800
hcrisbuc <cb@bucerzan.com>
2017-09-26 21:42:42 +0100

    added new files

--- pa/provision/provision.sh
+++ pa/provision/provision.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
-# vi /etc/sysconfig/network-scripts/ifcfg-eth0
-
-sudo su
+#!/udo su
 echo '''DEVICE="eth1"
         ONBOOT="yes"
-	        TYPE="Ethernet"
-		        BOOTPROTO="static"
-			        IPADDR="10.2.1.16"
-				        NETMASK="255.255.255.0" ''' > /etc/sysconfig/network-scripts/ifcfg-eth1
-
+	TYPE="Ethernet"
+        BOOTPROTO="static"
+	IPADDR="192.168.56.215"
+	NETMASK="255.255.255.0" ''' > /etc/sysconfig/network-scripts/ifcfg-eth1
+					
 					        /etc/init.d/network restart
+						
+						    echo '192.168.56.215 CentInf01' >> /etc/hostsin/bash
+
